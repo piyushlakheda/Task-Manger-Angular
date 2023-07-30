57b1ead54b461a8e86330965234dff1293ebde20
piyushlakheda <piyushlakheda@gmail.com>
2023-07-30 19:11:57 +0530

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { EditTodoComponent } from './MyComponents/edit-todo/edit-todo.component'
 
 const routes: Routes = [
   { path: 'todos', component: TodosComponent },
-  { path: 'edit-todo/:id', component: EditTodoComponent }, // Add this route for edit-todo component
+  { path: 'edit-todo/:id', component: EditTodoComponent }, 
   { path: '', redirectTo: '/todos', pathMatch: 'full' },
-  // Add other routes as needed
+
 ];
 
 @NgModule({
